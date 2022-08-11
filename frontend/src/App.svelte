b0f3d9a8084aa0013061d01c568cfc7792c5da0c
knackwurstking <knackwurstking.tux@gmail.com>
2022-08-11 15:06:48 +0200

    fiddle with the "app" layout?

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,44 +1,79 @@
 <script>
     import {
-        Disclosure,
-        DisclosurePanel,
-        DisclosureButton,
+        Popover,
+        PopoverButton,
+        PopoverPanel,
     } from "@rgossiaux/svelte-headlessui";
 
     import {
-        ChevronRightIcon,
+        ChevronDownIcon,
     } from "@rgossiaux/svelte-heroicons/solid";
 
-    import { fade } from "svelte/transition";
+    import { slide } from "svelte/transition";
 
-    let width = "40rem";
+    //let width = "40rem";
 
-    let sections = [
-        { id: 0, title: "Sections", open: true },
-        { id: 1, title: "Groups", open: false },
-        { id: 2, title: "Scripts", open: false },
+    let items = [
+        { id: 0, name: "Sections" },
+        { id: 1, name: "Groups" },
+        { id: 2, name: "Scripts" },
     ];
+    let selectedItem = items[0]
 </script>
 
-{#each sections as section (section.id)}
-  <Disclosure style={`width:${width};margin:0.5rem 0;`} let:open defaultOpen={section.open}>
-    <DisclosureButton style="width:100%;margin:0.5rem 0;">
-      <div style="display:flex;position:relative;align-items:center;justify-content:space-between;">
-        <div style="padding-right: 1rem">{section.title}</div>
-        <ChevronRightIcon
-            style={(open ? "transform: rotate(90deg);": "") + "width: 1.5rem"}
-        />
-      </div>
-    </DisclosureButton>
-    {#if open}
-      <div transition:fade>
-        <DisclosurePanel style="width:100%;margin: 0.5rem 0;padding:0.5rem">
-          <span>... here should be a list with {section.title.toLowerCase()} ...</span>
-        </DisclosurePanel>
-      </div>
-    {/if}
-  </Disclosure>
-{/each}
+<Popover
+  style={`
+     position: absolute;
+     top: 0;
+     left: 0;
+   `}
+  let:close
+>
+  <PopoverButton
+    style={`
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 10rem;
+      position: absolute;
+      top: 1rem;
+      left: 2rem;
+    `}
+  >
+    <span>{selectedItem.name}</span>
+    <ChevronDownIcon style="width:1.5rem;"/>
+  </PopoverButton>
+
+  <PopoverPanel
+    style={`
+      position: absolute;
+      z-index: 999;
+      background-color: var(--secondary-bg-color);
+      border-radius: var(--border-radius);
+      padding: 1rem;
+      border: 0.1rem solid;
+      border-color: var(--secondary-color);
+      width: 25rem;
+      top: 4.5rem;
+      left: 1.5rem;
+    `}
+  >
+    <!-- TODO: do some slide in transition from top to bottom -->
+    <div class="popover-panel-content" transition:slide>
+      {#each items as item (item.id)}
+        <a href="#" on:click={() => close()}>{item.name}</a>
+      {/each}
+    </div>
+  </PopoverPanel>
+</Popover>
 
 <style>
+  .popover-panel-content {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .popover-panel-content > * {
+    margin: 0.5rem 0;
+  }
 </style>
