05ad94eb661c3a0f50366ba5867678e266f7a127
knackwurstking <knackwurstking.tux@gmail.com>
2022-08-11 13:16:06 +0200

    disclosure: sections, groups and scripts (testing, styling, basics)

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,45 +1,44 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+    import {
+        Disclosure,
+        DisclosurePanel,
+        DisclosureButton,
+    } from "@rgossiaux/svelte-headlessui";
+
+    import {
+        ChevronRightIcon,
+    } from "@rgossiaux/svelte-heroicons/solid";
+
+    import { fade } from "svelte/transition";
+
+    let width = "40rem";
+
+    let sections = [
+        { id: 0, title: "Sections", open: true },
+        { id: 1, title: "Groups", open: false },
+        { id: 2, title: "Scripts", open: false },
+    ];
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+{#each sections as section (section.id)}
+  <Disclosure style={`width:${width};margin:0.5rem 0;`} let:open defaultOpen={section.open}>
+    <DisclosureButton style="width:100%;margin:0.5rem 0;">
+      <div style="display:flex;position:relative;align-items:center;justify-content:space-between;">
+        <div style="padding-right: 1rem">{section.title}</div>
+        <ChevronRightIcon
+            style={(open ? "transform: rotate(90deg);": "") + "width: 1.5rem"}
+        />
+      </div>
+    </DisclosureButton>
+    {#if open}
+      <div transition:fade>
+        <DisclosurePanel style="width:100%;margin: 0.5rem 0;padding:0.5rem">
+          <span>... here should be a list with {section.title.toLowerCase()} ...</span>
+        </DisclosurePanel>
+      </div>
+    {/if}
+  </Disclosure>
+{/each}
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
