b82e6bf6534fa67397d16ac011a52f359db3b194
knackwurstking <knackwurstking.tux@gmail.com>
2022-08-13 19:57:48 +0200

    Handling cors for api endpoint(s)

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-chi/cors v1.2.1
 	github.com/sirupsen/logrus v1.9.0
 	gopkg.in/yaml.v3 v3.0.1
 )
--- internal/router/api.go
+++ internal/router/api.go
@@ -6,11 +6,24 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"gitlab.com/knackwurstking/pirgb-web/internal/config"
 )
 
 func init() {
 	Mux.Route("/api", func(r chi.Router) {
+		r.Use(cors.Handler(cors.Options{
+			//AllowedOrigins: []string{"https://foo.com"}, // use this to allow specific origin hosts
+			AllowedOrigins: []string{"https://*", "http://*"},
+			//AllowOriginFunc: func(r *http.Request, origin string) bool { return true }, // allow all
+			AllowedMethods: []string{"GET"},
+			AllowedHeaders: []string{},
+			//ExposedHeaders:   []string{"Content-Type"},
+			ExposedHeaders:   []string{},
+			AllowCredentials: false,
+			//MaxAge:           300, // Maximum value not ignored by any of major browsers
+		}))
+
 		r.Get("/sections", getSectionsHandler)
 		r.Get("/groups", getGroupsHandler)
 	})
