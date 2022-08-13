177e294225b089177d33ea2eb80ccd3f6492c074
knackwurstking <knackwurstking.tux@gmail.com>
2022-08-13 19:44:31 +0200

    handle api: GET sections and groups

--- internal/router/api.go
+++ internal/router/api.go
@@ -2,15 +2,35 @@
 package router
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
+	"gitlab.com/knackwurstking/pirgb-web/internal/config"
 )
 
 func init() {
 	Mux.Route("/api", func(r chi.Router) {
-		// TODO: - sections => GET: "/api/sections"
-		// TODO: - groups   => GET: "/api/groups"
+		r.Get("/sections", getSectionsHandler)
+		r.Get("/groups", getGroupsHandler)
 	})
 
 	info["GET /api/sections"] = "get all sections"
 	info["GET /api/groups"] = "get all groups"
+}
+
+func getSectionsHandler(w http.ResponseWriter, r *http.Request) {
+	// sections => GET: "/api/sections"
+	w.Header().Add("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(config.GlobalData.Sections); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func getGroupsHandler(w http.ResponseWriter, r *http.Request) {
+	// groups   => GET: "/api/groups"
+	w.Header().Add("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(config.GlobalData.Groups); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
