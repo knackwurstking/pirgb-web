877f054ff63f08a325a37b62978f4ed884402cb0
knackwurstking <knackwurstking.tux@gmail.com>
2022-08-14 18:34:10 +0200

    Scan sections if len == 0

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"path/filepath"
+	"sync"
 
 	"github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v3"
@@ -85,12 +86,36 @@ func DoIt() {
 	if err != nil {
 		logrus.Warnf("Read config failed: %s", err.Error())
 	} else {
+		// merge config into `GlobalData` (if possible)
 		err = yaml.Unmarshal(data, GlobalData)
 		if err != nil {
 			logrus.Warnf("Load config failed: %s", err.Error())
 		}
 	}
 
-	// TODO: scan and parse sections and groups data (using pirgb v0.2.0)
-	// TODO: on error: remove section from configuration
+	go func() {
+		var wg sync.WaitGroup
+
+		// scan and parse sections and groups data (using pirgb v0.2.0)
+		for _, section := range GlobalData.Sections {
+			if len(section.Sections) == 0 {
+				wg.Add(1)
+				go func(wg *sync.WaitGroup) {
+					defer wg.Done()
+					// scan for sections ("/info" endpoint)
+					if err := section.Scan(); err != nil {
+						// TODO: remove section from the list and continue (and log)
+						// ...
+					} else if len(section.Sections) == 0 {
+						// TODO: if no sections found also remove section from the list (and log)
+						// ...
+					}
+				}(&wg)
+			}
+		}
+
+		wg.Wait()
+		// TODO: parse groups if scan has finished
+		// ...
+	}()
 }
