f714ae87ce2007cf9c7b51cdf2d91f488a4dfbc5
Udo Bauer <knackwurstking.tux@gmail.com>
2022-08-15 17:05:26 +0200

    Add `GetGroups` method

--- internal/config/groups.go
+++ internal/config/groups.go
@@ -2,7 +2,32 @@ package config
 
 // Groups ...
 type Groups struct {
-	Devices *Devices
+	Devices *Devices `json:"-"`
+}
+
+// GetGroups for all devices
+func (g *Groups) GetGroups() []string {
+	var groups []string
+
+	for _, device := range *g.Devices {
+		for _, group := range device.Groups {
+			// check if group already in groups
+			var exists bool
+
+			for _, g := range groups {
+				if g == group {
+					exists = true
+					break
+				}
+			}
+
+			if !exists {
+				groups = append(groups, group)
+			}
+		}
+	}
+
+	return groups
 }
 
 // TODO: list groups available: []string
