a53a2fb50811c632965a6825119db9918320da6b
Udo Bauer <knackwurstking.tux@gmail.com>
2022-08-15 18:08:06 +0200

    Adding server proxy for "/api"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [svelte()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://pc:50831",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+});
