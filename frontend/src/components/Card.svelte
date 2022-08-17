c7920a5fe4e68835d646a80e417546b40d692763
Udo Bauer <knackwurstking.tux@gmail.com>
2022-08-17 02:52:19 +0200

    (fieldset) card like component

---
+++ frontend/src/components/Card.svelte
@@ -0,0 +1,71 @@
+<script>
+  import * as api from "../lib/api"
+
+  export let section;
+
+  /** @type {number} */
+  export let pulse = 100 // TODO: get device section data (pwm) and set Pulse and RGBW
+  $: pulse > 100 ? (pulse = 100) : pulse < 0 && (pulse = 0)
+</script>
+
+<fieldset class="section card">
+  <legend class="title">{section.Host}</legend>
+  <pre>[Section: {section.SectionID}, Port: {section.Port}]</pre>
+
+  <!-- TODO: Actions: Pulse / RGBW -->
+  <label class="input">
+    <span>Pulse</span>
+    <input
+      class="pulse"
+      type="number"
+      min={0} max={100}
+      bind:value={pulse}
+    />
+  </label>
+
+  <button
+    class="on"
+    on:click={() => {
+      api.setPWM(section.Host, section.SectionID, {
+        pulse: pulse, rgbw: [255,255,255,255],
+      })
+    }}
+  >
+    ON
+  </button>
+  <button
+    class="off"
+    on:click={() => {
+      api.setPWM(section.Host, section.SectionID, { pulse: 0 })
+    }}
+  >
+    OFF
+  </button>
+
+</fieldset>
+
+<style>
+  fieldset {
+    margin: 1em 0;
+  }
+
+  fieldset button {
+    margin: 0.5em;
+  }
+
+  label.input {
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+    margin: 0.5em 0;
+  }
+
+  label.input span {
+    font-size: 0.70rem;
+    text-decoration: underline;
+  }
+
+  label.input input {
+    text-align: center;
+  }
+</style>
