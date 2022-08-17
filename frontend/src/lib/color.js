301453ed6ca5caf016b77471c6bb77cb6bc0a405
Udo Bauer <knackwurstking.tux@gmail.com>
2022-08-17 15:46:24 +0200

    calc hex to rgbw and leave a comment (todo)

--- frontend/src/lib/color.js
+++ frontend/src/lib/color.js
@@ -1,8 +1,30 @@
 /** @param {string} color */
-export function hexToRGBW(color) {
-  // TODO: convert a hex color value to rgb(w)
+export function hexToRGBW(color, fix = true) {
+  // prevent errors
+  if (color[0] !== "#" || color.length < 7) return [255, 255, 255, 255]
 
-  return [255, 255, 255, 255] // placeholder
+  /** @type {number[]} */
+  let rgbw = []
+  color = color.slice(1) // remove "#" from color
+
+  for (let x = 1; x < color.length; x++) {
+    if (x % 2 == 1) {
+      rgbw.push(parseInt(`${color[x - 1]}${color[x]}`, 16))
+    }
+  }
+
+  // push W to rgbw
+  if (rgbw.length < 4) {
+    if (!rgbw.filter(v => v != rgbw[0]).length) {
+      rgbw.push(rgbw[0])
+    } else {
+      rgbw.push(0)
+    }
+  }
+
+  // TODO: Fix color (ex: [192,191, 188]) - diff <= 5 should be all even (the highest value)
+
+  return rgbw // placeholder
 }
 
 /** @param {number[]} rgbw */
