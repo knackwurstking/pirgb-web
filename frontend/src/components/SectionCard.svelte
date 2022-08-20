c8010e367a1f710bb0828e2546c6f4494ee34993
Udo Bauer <knackwurstking.tux@gmail.com>
2022-08-20 22:45:51 +0200

    @TODO SectionCard will replace the old Card component

---
+++ frontend/src/components/SectionCard.svelte
@@ -0,0 +1,141 @@
+<script>
+  import * as api from "../lib/api"
+  import * as utils from "../lib/utils"
+
+  import { onMount } from "svelte"
+
+  /** @type {string} */
+  export let host
+  /** @type {number} */
+  export let port
+  /** @type {number} */
+  export let sectionID 
+
+  /** @type {number} */
+  export let pulse = 0
+  $: pulse > 100 ? (pulse = 100) : pulse < 0 && (pulse = 0)
+
+  export let color = "#ffffff"
+
+  onMount(() => {
+    console.log(`[Card.svelte] [onMount] ${host}:${port} [sectionID ${sectionID}]`)
+    refresh(null)
+  })
+
+  // TODO: Need to handle color differently ...
+  //  - calculate pulse from color
+
+  /**
+   * @param {import('../lib/api').Section} section
+   */
+  async function refresh(section = null) {
+    console.log(`[Card.svelte] [refresh] host=${host} sectionID=${sectionID}`)
+    if (!section) section = await api.getPWM(host, sectionID)
+    let newColor = "#"
+    for (let idx=0; idx < section.pins.length; idx++) {
+      if (section.pins[idx].pulse)
+        pulse = section.pins[idx].pulse
+
+      if (idx < 3)
+        newColor += section.pins[idx].colorValue.toString(16).padStart(2, "0")
+    }
+
+    if (!pulse) pulse = 100
+    color = newColor
+  }
+</script>
+
+<fieldset class="section card">
+  <legend class="title">{host}</legend>
+  <pre>[Section: {sectionID}, Port: {port}]</pre>
+
+  <input
+    type="color"
+    bind:value={color}
+  />
+
+  <section class="actions">
+    <label class="input">
+      <span>Pulse</span>
+      <input
+        class="pulse"
+        type="number"
+        min={0} max={100}
+        bind:value={pulse}
+        on:focus={(ev) => {
+          // @ts-ignore
+          ev.target.select()
+        }}
+      />
+    </label>
+
+    <button
+      class="on"
+      on:click={() => {
+        api.setPWM(host, sectionID,
+          { pulse: pulse, rgbw: utils.colorToRGBW(color),
+        })
+      }}
+    >
+      ON
+    </button>
+    <button
+      class="off"
+      on:click={() => {
+        api.setPWM(host, sectionID,
+          { pulse: 0, rgbw: utils.colorToRGBW(color) })
+      }}
+    >
+      OFF
+    </button>
+  </section>
+
+</fieldset>
+
+<style>
+  fieldset {
+    margin: 1em 0;
+  }
+
+  fieldset > pre {
+    margin-top: 0em;
+  }
+
+  section.actions {
+    display: flex;
+    place-items: center;
+  }
+
+  section.actions > * {
+    margin: 0.5rem;
+  }
+
+  section.actions > button {
+    font-size: 1rem;
+    height: fit-content;
+    margin-top: 1rem;
+  }
+
+  label.input {
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+  }
+
+  label.input span {
+    font-size: 0.70rem;
+    text-decoration: underline;
+  }
+
+  label.input input {
+    text-align: center;
+    width: 5em;
+  }
+
+  input[type=color] {
+    width: 90%;
+    height: 2.5em;
+    max-width: 10em;
+    padding: 0.1em;
+  }
+</style>
