79a116854ab66b0bf66b8cd66bd479c01081b398
Udo Bauer <knackwurstking.tux@gmail.com>
2022-08-22 16:32:38 +0200

    Heartbeat for checking if still connected

--- frontend/src/lib/events.js
+++ frontend/src/lib/events.js
@@ -28,40 +28,86 @@ class GlobalEvents extends EventTarget {
     /** @type {WebSocket} */
     this.ws = null
 
+    /** @type {number|NodeJS.Timeout} */
+    this._timeout = 0
+
+    this._heartbeatTimeoutValue = 2500
+
+
+    this._NONE = 0
+    this._SEND = 1
+    this._RECEIVED = 2
+    this._FAILED = 3
+
+    /**
+     * 0: none
+     * 1: send
+     * 2: received
+     * 3; fail
+     */
+    this._heartbeat = 0 
+
     this.connect()
   }
 
   // Connect to "/api/events"
   connect() {
-    // TODO: websocket ping pong for detect a disconnect
     if (this.ws) {
       this.ws.close()
     }
 
-    this.ws = new WebSocket(`${location.protocol === "https:" ? "wss" : "ws"}://${window.location.host}/api/events`)
-    window.ws = this.ws
+    this.ws = new WebSocket(
+      `${location.protocol === "https:" ? "wss" : "ws"}://${window.location.host}/api/events`
+    )
 
     this.ws.onopen = (ev) => {
       console.log("[events] [onopen]", ev)
       this.dispatchCustomEvent("open", null)
+      this.heartbeat()
     }
 
     this.ws.onclose = (ev) => {
       console.log("[events] [onclose]", ev)
+      clearTimeout(this._timeout)
       this.dispatchCustomEvent("close", null)
-    }
-
-    this.ws.onerror = (ev) => {
-      console.log("[events] [onerror]", ev)
     }
 
     this.ws.onmessage = (ev) => {
       console.log("[events] [onmessage]", ev)
+      if (ev.data == "heartbeat") {
+        if (this._heartbeat === this._FAILED) {
+          this.dispatchCustomEvent("open", null)
+        }
+
+        this._heartbeat = this._RECEIVED
+        return
+      }
 
       /** @type {ServerEventData} */
       const eventData = JSON.parse(ev.data)
       this.dispatchCustomEvent(eventData.name, eventData.data)
     }
+  }
+
+  heartbeat() {
+    if (this._timeout) {
+      clearTimeout(this._timeout)
+      this._timeout = 0
+    }
+
+    if (!this.ws) return
+    if (this.ws.readyState !== this.ws.OPEN) return
+
+    if (this._heartbeat === this._SEND) {
+      this._heartbeat = this._FAILED
+      this.closed = true
+      this.dispatchCustomEvent("close", null)
+    }
+
+    console.log("[events] send a heartbeat ...")
+    if (this._heartbeat !== this._FAILED) this._heartbeat = this._SEND
+    this.ws.send("heartbeat")
+    this._timeout = setTimeout(this.heartbeat.bind(this), this._heartbeatTimeoutValue);
   }
 
   /**
