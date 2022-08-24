e4c32187ec6858463ad609b1712af77f62f17b60
Udo Bauer <knackwurstking.tux@gmail.com>
2022-08-24 21:50:29 +0200

    seletton

---
+++ frontend/src/components/PulseSlider.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let min = 0
+  export let max
+  export let value = 0
+</script>
+
+<label>
+  <span>{value}</span>
+  <input
+    type="range"
+    {min}
+    {max}
+    bind:value
+  />
+</label>
+
+<style>
+  label {
+  }
+
+  label span {
+  }
+
+  label input {
+  }
+</style>
