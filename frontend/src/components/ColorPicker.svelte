458c915905d861bac3e0274387c257bad35fd2c8
Udo Bauer <knackwurstking.tux@gmail.com>
2022-08-24 20:47:25 +0200

    undo this white color stuff

--- frontend/src/components/ColorPicker.svelte
+++ frontend/src/components/ColorPicker.svelte
@@ -6,15 +6,16 @@
    * @type {string[][]}
    */
   export let colors = [
-    ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ff00ff', '#ffffff'], // 255 (ff)
-    ['#ff3f00', '#3fff00', '#00ff3f', '#003fff', '#3f00ff', '#ff003f', '#ffffff'], // 63  (3f)
-    ['#ff7f00', '#7fff00', '#00ff7f', '#007fff', '#7f00ff', '#ff007f', '#ffffff'], // 127 (7f)
-    ['#ffbf00', '#bfff00', '#00ffbf', '#00bfff', '#bf00ff', '#ff00bf', '#ffffff'], // 191 (bf)
+    ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ff00ff'], // 255 (ff)
+    ['#ff3f00', '#3fff00', '#00ff3f', '#003fff', '#3f00ff', '#ff003f'], // 63  (3f)
+    ['#ff7f00', '#7fff00', '#00ff7f', '#007fff', '#7f00ff', '#ff007f'], // 127 (7f)
+    ['#ffbf00', '#bfff00', '#00ffbf', '#00bfff', '#bf00ff', '#ff00bf'], // 191 (bf)
     // ...
-    ['#ff7f7f', '#ffff7f', '#7fff7f', '#7fffff', '#7f7fff', '#ff7fff', '#ffffff'], // 7f
-    ['#ff3f7f', '#3fff7f', '#7fff3f', '#7f3fff', '#3f7fff', '#ff7f3f', '#ffffff'], // 3f,7f
-    ['#ff3fbf', '#3fffbf', '#bfff3f', '#bf3fff', '#3fbfff', '#ffbf3f', '#ffffff'], // 3f,bf
-    ['#ff7fbf', '#7fffbf', '#bfff7f', '#bf7fff', '#7fbfff', '#ffbf7f', '#ffffff'], // 7f,bf
+    ['#ff7f7f', '#ffff7f', '#7fff7f', '#7fffff', '#7f7fff', '#ff7fff'], // 7f
+    ['#ff3f7f', '#3fff7f', '#7fff3f', '#7f3fff', '#3f7fff', '#ff7f3f'], // 3f,7f
+    ['#ff3fbf', '#3fffbf', '#bfff3f', '#bf3fff', '#3fbfff', '#ffbf3f'], // 3f,bf
+    ['#ff7fbf', '#7fffbf', '#bfff7f', '#bf7fff', '#7fbfff', '#ffbf7f'], // 7f,bf
+    ['#ffffff'],
   ]
 
   // Initial value
@@ -199,7 +200,7 @@
   }
 	
   .values-dropdown-grid {
-      grid-template-columns: repeat(7, 1.5rem);
+      grid-template-columns: repeat(6, 1.5rem);
       grid-template-rows: 1.5rem 1.5rem;
       grid-gap: 0.625rem;
       display: grid;
