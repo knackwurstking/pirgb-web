2bda4878726e84722598be825769e4cb4cd06b49
Udo Bauer <knackwurstking.tux@gmail.com>
2022-08-25 11:25:41 +0200

    add vite plugin "remove console logs"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), removeConsole()],
   server: {
     proxy: {
       "/api": {
