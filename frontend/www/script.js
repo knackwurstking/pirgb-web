9c1d2fa39bedde8c3b8d070abab48ebee50d0886
Udo Bauer <knackwurstking.tux@gmail.com>
2022-09-21 17:04:47 +0200

    some basic and simple section card

--- frontend/www/script.js
+++ frontend/www/script.js
@@ -1,5 +1,14 @@
 "use strict";
 
+// TODO: color picker
+// TODO: limit the range slider min based on color value picked
+// TODO: power toggle component
+// TODO: from backend: init and startwebsocket handler...
+//    ...also handle suspend if possible...
+//    ...after suspend get update from server (devices/sections)
+// TODO: from backend: get devices and sections stuff
+// TODO: update sectionsContainer
+
 /** @type {HTMLDivElement} */
 const sectionsContainer = document.getElementById("sectionsContainer");
 
@@ -7,12 +16,8 @@ const sectionsContainer = document.getElementById("sectionsContainer");
 const template = document.getElementById("sectionCard");
 
 /** @type {DocumentFragment} */
-const section = sectionsContainer.appendChild(template.content.cloneNode(true));
-console.log("section:", section);
-
-/** @type {HTMLParagraphElement} */
-const section2 = sectionsContainer.appendChild(
-  template.content.cloneNode(true).querySelector("p")
+const section = sectionsContainer.appendChild(
+  template.content.cloneNode(true).querySelector("fieldset")
 );
-section2.innerText = "What's up!";
-console.log("section2:", section2);
+section.querySelector("legend > span").innerText = "pi-bed";
+section.querySelector("legend > code").innerText = "[0]";
