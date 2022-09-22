b4c532e10764c462bd90bd66b457a26e1214e3c8
Udo Bauer <knackwurstking.tux@gmail.com>
2022-09-22 20:08:30 +0200

    update dependencies...
    - remove sass
    - remove svelte-preprocess

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from "vite"
-import { svelte } from "@sveltejs/vite-plugin-svelte"
-import removeConsole from "vite-plugin-remove-console"
-import sveltePreprocess from "svelte-preprocess"
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import removeConsole from "vite-plugin-remove-console";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte({ preprocess: [sveltePreprocess()] }), removeConsole()],
+  plugins: [svelte(), removeConsole()],
   server: {
     proxy: {
       "/api": {
@@ -16,4 +14,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
