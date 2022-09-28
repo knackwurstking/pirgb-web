b5c3e88c6b82420ca16eb9cb1335889bd094fef2
Udo Bauer <knackwurstking.tux@gmail.com>
2022-09-28 02:19:32 +0200

    update ubports build and frontend location js - handling file: protocol (improvised)

--- frontend/src/js/location.js
+++ frontend/src/js/location.js
@@ -1,8 +1,11 @@
 export default (function () {
+  // TODO: "rpi-server" 50831 and "http:" are just placeholder for now
+  // TODO: need to handle the file: protocol (open settings page for set server url)
   const data = {
-    hostname: window.location.hostname,
-    port: window.location.port,
-    protocol: window.location.protocol,
+    hostname: window.location.hostname || "rpi-server",
+    port: window.location.port || 50831,
+    protocol:
+      window.location.protocol === "file:" ? "http:" : window.location.protocol,
 
     get origin() {
       return `${this.protocol}//${this.hostname}:${this.port}`;
