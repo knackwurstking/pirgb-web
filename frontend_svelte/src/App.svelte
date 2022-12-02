016a3bfe13bf54239a86d4ee9729488462eff36b
Udo Bauer <knackwurstkin.tux@gmail.com>
2022-12-02 01:49:44 +0100

    Update tailwind and some testing

--- frontend_svelte/src/App.svelte
+++ frontend_svelte/src/App.svelte
@@ -1,54 +1,13 @@
 <script>
-  import svelteLogo from "./assets/svelte.svg";
-  import Counter from "./lib/Counter.svelte";
-  import { Alert } from "flowbite-svelte";
+  import { P } from "flowbite-svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <div class="p-8">
-    <Alert>
-      <span class="font-medium">Info Alert!</span> Change a few things ...
-    </Alert>
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+  <P class="m-6 p-6 w-fit border border-gray-200"
+    >@todo: Accordion sections and groups and settings OR just a standard app
+    layout with Appbar/Toolbar drawer and view</P
+  >
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
