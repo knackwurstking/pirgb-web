63b5003ddfada1bfc9115e947cac95323f9fd0ba
Udo Bauer <knackwurstking.tux@gmail.com>
2022-12-08 21:49:23 +0100

    Never nester code clean up

--- pkg/middleware/logger.go
+++ pkg/middleware/logger.go
@@ -9,15 +9,16 @@ import (
 
 // Logger - endpoint logger
 func Logger(h http.HandlerFunc) http.HandlerFunc {
+	log := func(w *ResponseWriter, r *http.Request, start time.Time) {
+		log.Info.Printf("%-6s %s from %s - %d %s - %s",
+			r.Method, r.URL.Path, r.RemoteAddr,
+			w.Status, http.StatusText(w.Status),
+			time.Since(start).String())
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		ww := GetCachedResponse(w)
-
-		defer func(ww *ResponseWriter, start time.Time) {
-			log.Info.Printf("%-6s %s from %s - %d %s - %s",
-				r.Method, r.URL.Path, r.RemoteAddr,
-				ww.Status, http.StatusText(ww.Status),
-				time.Since(start).String())
-		}(ww, time.Now())
+		defer log(ww, r, time.Now())
 
 		h.ServeHTTP(ww, r)
 	}
