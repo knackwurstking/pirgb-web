1780afc4464fccb98a3f633db1874af819a250d6
Udo Bauer <knackwurstking.tux@gmail.com>
2022-12-09 13:50:47 +0100

    Remove todo and add some types to *env.d.ts

--- frontend/src/pages/Sections.svelte
+++ frontend/src/pages/Sections.svelte
@@ -3,7 +3,6 @@
 
   onMount(() => {
     console.log("/sections");
-    // TODO: need a proxy to the backend server
     // TODO: request sections from backend
   });
 </script>
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,2 +1,18 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+interface Section {
+  id: number;
+  pulse?: number;
+  lastPulse?: number;
+  // TODO: ...
+}
+
+interface Device {
+  host: string;
+  port: number;
+  sections: Section[];
+  groups: string[];
+}
+
+type Devices = Device[];
