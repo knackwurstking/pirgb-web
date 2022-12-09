d15c0e321092ef37406555aae0a0676a8f047693
Udo Bauer <knackwurstking.tux@gmail.com>
2022-12-09 13:02:55 +0100

    Remove tailwind and flowbite; add navbar and routing

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,13 +1,68 @@
 <script>
-  import { P } from "flowbite-svelte";
+  import { Router, Route, links, navigate as _navigate } from "svelte-routing";
+
+  import Home from "./pages/Home.svelte";
+  import Sections from "./pages/Sections.svelte";
+  import Groups from "./pages/Groups.svelte";
+  import Settings from "./pages/Settings.svelte";
+
+  let url = location.pathname;
+
+  /**
+   * @param {string} _url
+   */
+  function navigate(_url) {
+    _navigate(url);
+    url = _url;
+  }
 </script>
 
-<main>
-  <P class="m-6 p-6 w-fit border border-gray-200"
-    >@todo: Accordion sections and groups and settings OR just a standard app
-    layout with Appbar/Toolbar drawer and view</P
-  >
+<main use:links>
+  <Router>
+    <nav>
+      <button
+        class="link"
+        class:active={url === "/sections"}
+        on:click={() => navigate("/sections")}
+      >Home</button>
+      <button
+        class="link"
+        class:active={url === "/groups"}
+        on:click={() => navigate("/groups")}
+      >Groups</button>
+      <button
+        class="link"
+        class:active={url === "/settings"}
+        on:click={() => navigate("/settings")}
+      >Settings</button>
+    </nav>
+    <div>
+      <Route path="/sections" component={Sections} />
+      <Route path="/groups" component={Groups} />
+      <Route path="/settings" component={Settings} />
+      <Route path="/" component={Home} />
+    </div>
+  </Router>
 </main>
 
 <style>
+  nav {
+    height: 50px;
+    display: flex;
+    place-items: center;
+    justify-content: flex-end;
+    border-bottom: 1px solid currentColor;
+  }
+
+  nav button.link {
+    background: transparent;
+    border: none;
+    outline: none;
+    margin: 4px 16px;
+    color: var(--color-link);
+  }
+
+  nav button.link.active {
+    color: var(--color-link--active);
+  }
 </style>
---
+++ frontend/src/pages/Sections.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    console.log("/sections");
+  });
+</script>
+
+<!-- TODO: render sections here (list style) -->
