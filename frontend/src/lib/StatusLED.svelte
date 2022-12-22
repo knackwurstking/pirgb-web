152f37c9b21d93639f389686cfb16e0c3677d6b6
Udo Bauer <knackwurstking.tux@gmail.com>
2022-12-22 08:42:56 +0100

    Status led color sizing (*--inner)

--- frontend/src/lib/StatusLED.svelte
+++ frontend/src/lib/StatusLED.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="status-led--outer" class:active {...$$restProps}>
-    <span
+    <div
         style={`
             background-color: ${active ? activeColor : inactiveColor};
         `}
@@ -14,19 +14,20 @@
 </div>
 
 <style>
-    /* inactive styles */
     .status-led--outer {
         position: relative;
-        border: 50%;
-        padding: 8px;
+        padding: 4px;
         border: 1px solid var(--color-border);
-        width: 1rem;
-        height: 1rem;
+        border-radius: 50%;
+        width: calc(1rem + 8px);
+        height: calc(1rem + 8px);
     }
 
-    /* active styles */
     .status-led--outer.active .status-led--inner {
+        border-radius: 50%;
         width: 100%;
         height: 100%;
+        filter: blur(0.125rem);
+        /* TODO: pulse animation blur value up and down */
     }
 </style>
